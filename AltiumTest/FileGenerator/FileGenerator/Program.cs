2dc6dab3d2e1f0c4a1d6755ee97d77fb2b4668a9
karoljaworowski <karol.jaworowski@gmail.com>
2023-02-14 14:46:30 +0100

    Add files via upload

---
+++ AltiumTest/FileGenerator/FileGenerator/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bogus;
+using ShellProgressBar;
+
+Console.WriteLine("Hello, World!");
+
+var faker = new Faker();
+
+Console.WriteLine("How many words do you want to draw");
+int wordsNumer;
+while (!int.TryParse(Console.ReadLine(), out wordsNumer))
+{
+    Console.WriteLine("How many words do you want to draw");
+}
+
+Console.WriteLine("Number of lines in file");
+int linesNumber;
+while (!int.TryParse(Console.ReadLine(), out linesNumber))
+{
+    Console.WriteLine("Number of lines");
+}
+
+Console.WriteLine("Range of numbers to draw");
+int range;
+while (!int.TryParse(Console.ReadLine(), out range))
+{
+    Console.WriteLine("Range of numbers");
+}
+
+var listOfWords = new List<string>();
+for (int i = 0; i < wordsNumer; i++)
+{
+    var word = $"{faker.Vehicle.Model()} - {faker.Vehicle.Fuel()}";
+    Console.WriteLine(word);
+    listOfWords.Add(word);
+}
+
+var randomNumber = new Random();
+var randomWord = new Random();
+Directory.CreateDirectory("C:/Karol");
+using (StreamWriter file = new("C:/Karol/input.txt"))
+{
+    var options = new ProgressBarOptions
+    {
+        ProgressCharacter = '─',
+        ProgressBarOnBottom = true
+    };
+    using (var pbar = new ProgressBar(linesNumber, "File generating", options))
+    {
+        for (int i = 0; i < linesNumber; i++)
+        {
+            var line = $"{randomNumber.Next(range)}. {listOfWords[randomWord.Next(wordsNumer)]} ";
+            await file.WriteLineAsync(line);
+            pbar.Tick();
+        }
+    }
+}
---
+++ AltiumTest/FileSorter/Program.cs
@@ -0,0 +1,72 @@
+﻿using ShellProgressBar;
+using System.Data;
+
+Console.WriteLine("Sort data");
+
+var listOfFiles = new List<string>();
+
+var partsFilesPath = "C:/Karol/Parts";
+if (Directory.Exists(partsFilesPath))
+{
+    Directory.Delete(partsFilesPath, true);
+}
+Directory.CreateDirectory(partsFilesPath);
+
+var path = "C:/Karol/Parts/{0}_input.txt";
+var outputPath = "C:/Karol/output.txt";
+if (File.Exists(outputPath))
+{
+    File.Delete(outputPath);
+}
+var options = new ProgressBarOptions
+{
+    ProgressCharacter = '─',
+    ProgressBarOnBottom = true
+};
+
+var totalLines = File.ReadLines(@"C:/Karol/input.txt").Count();
+
+using (var pbar = new ProgressBar(totalLines, "Splitting data", options))
+{
+    foreach (string line in File.ReadLines(@"C:/Karol/input.txt"))
+    {
+        var data = line.Split(". ");
+        var prefix = data[1].Substring(0, 5);
+        var filePath = string.Format(path, prefix);
+        using (StreamWriter sw = File.AppendText(filePath))
+        {
+            sw.WriteLine(line);
+        }
+        pbar.Tick();
+        pbar.Message = $"Splitting data - {data[1]}";
+    }
+}
+
+var fileOrderdList = new DirectoryInfo(partsFilesPath)
+    .GetFileSystemInfos()
+    .OrderBy(file => file.Name)
+    .ToList();
+
+using (StreamWriter sw = File.AppendText(outputPath))
+{
+    using (var pbar = new ProgressBar(fileOrderdList.Count, "Sorting data", options))
+    {
+        foreach (var file in fileOrderdList)
+        {
+            pbar.Message = $"File '{file.Name}' processing";
+            var dataList = File.ReadAllLines(file.FullName)
+                .Select(line => line.Split(". "))
+                .Select(data => new Tuple<int, string>(int.Parse(data[0]), data[1]))
+                .OrderBy(data => data.Item2).ThenBy(data => data.Item1)
+                .ToList();
+
+            foreach (var data in dataList)
+            {
+                var line = $"{data.Item1}. {data.Item2}";
+                sw.WriteLine(line);
+            }
+            pbar.Tick();
+        }
+    }
+}
+
